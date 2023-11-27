624c9ffda9ed21c944134172d73080242d399a56
Zac Nation <zac.nation@icloud.com>
2023-11-27 12:16:33 +0000

    Build board

--- src/App.js
+++ src/App.js
@@ -1,3 +1,23 @@
-export default function Square() {
-  return <button className="square">X</button>;
+import Square from "./components/Square";
+
+export default function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  );
 }
