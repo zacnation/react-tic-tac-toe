ca1d42e0866df78cac6fb4cef7c4ef30399786ea
Zac Nation <zac.nation@icloud.com>
2023-11-27 12:33:51 +0000

    Add state

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,3 +1,14 @@
-export default function Square({ value }) {
-  return <button className="square">{value}</button>;
+import { useState } from "react";
+
+export default function Square() {
+  const [value, setvalue] = useState(null);
+
+  function handleClick() {
+    setvalue("X");
+  }
+  return (
+    <button className="square" onClick={handleClick}>
+      {value}
+    </button>
+  );
 }
