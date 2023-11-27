e944ee2545e047e211123f7dc7575ee006ed7908
Zac Nation <zac.nation@icloud.com>
2023-11-27 12:19:41 +0000

    Add props

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,3 +1,3 @@
-export default function Square() {
-  return <button className="square">X</button>;
+export default function Square({ value }) {
+  return <button className="square">{value}</button>;
 }
